15041515f713ea1d70ff882c3aa8203f9fd47c6f
alxlgtn <muteproj@gmail.com>
2020-12-07 14:36:16 +0300

    14_lesson css_module two class in one teg

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import stl from "./Navbar.module.css";
 
+console.log(stl);
+
 const Navbar = () => {
     return <nav className={stl.nav}>
-        <a className={stl.item } href="./">home</a>
+        <a className={stl.item} href="./">home</a>
         <a className={stl.item} href="./">about</a>
         <a className={stl.item} href="./">bio</a>
         <a className={stl.item} href="./">portfolio</a>
-        <a className={stl.item} href="./">contacts</a>
+        <a className={`${stl.item} ${stl.green}`} href="./">contacts</a>
     </nav>
 }
 
