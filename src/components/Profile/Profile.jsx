7102e2ab595056a4604cfa31750f618cd9cc5cbb
alxlgtn <muteproj@gmail.com>
2021-01-29 13:37:14 +0300

    Revert "brackepoint"
    This reverts commit ab2f713950679e14d6b888d8708340e6af478a16.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,10 +9,17 @@ import Posts from "./Posts/Posts";
 const Profile = (props) => {
     return (
         <div className={stl.content}>
+<<<<<<< HEAD
             <About aboutData={props.aboutData} />
             <Galery galeryData={props.galeryData}/>
             <SubmitPost submitPostData={props.submitPostData}/>
             <Posts postsData={props.postsData} />
+=======
+            <Galery galeryData={props.state.galeryData}/>
+            <About aboutData={props.state.aboutData} />
+            <SubmitPost submitPostData={props.state.submitPostData}/>
+            <Posts postsData={props.state.postsData} />
+>>>>>>> parent of ab2f713... brackepoint
         </div>
     );
 }
