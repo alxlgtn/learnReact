3b54088a57694d9c05376f17e89ce0800964c96d
alxlgtn <muteproj@gmail.com>
2021-01-29 13:32:42 +0300

    brackpoint 2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,21 +9,10 @@ import Posts from "./Posts/Posts";
 const Profile = (props) => {
     return (
         <div className={stl.content}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <About aboutData={props.state.aboutData} />
-=======
->>>>>>> parent of ab2f713... brackepoint
-            <Galery galeryData={props.state.galeryData}/>
-            <About aboutData={props.state.aboutData} />
-            <SubmitPost submitPostData={props.state.submitPostData}/>
-            <Posts postsData={props.state.postsData} />
-=======
-            <Galery galeryData={props.galeryData}/>
             <About aboutData={props.aboutData} />
+            <Galery galeryData={props.galeryData}/>
             <SubmitPost submitPostData={props.submitPostData}/>
             <Posts postsData={props.postsData} />
->>>>>>> parent of dae5651... 29 lesson - state
         </div>
     );
 }
