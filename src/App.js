2a7148dc176b864e16e72698c0a30b9f0e82ced1
alxlgtn <muteproj@gmail.com>
2021-02-05 23:43:20 +0300

    pause

--- src/App.js
+++ src/App.js
@@ -18,38 +18,15 @@ import {Route} from "react-router-dom"
 const App = (props) => {
     return (
         <BrowserRouter>
-            <div className="app-wrapper">                
-<<<<<<< HEAD
+            <div className="app-wrapper">
                 <Header state = {props.state.partHeader} />
-                <Navbar state = {props.state.partNavbar}/>
-                <Route path='/profile' render = { () => <Profile state = {props.state.pageProfile} /> }/>
-                <Route path='/messenger' render = { () => <Dialogs state = {props.state.pageMessenger} /> }/>
+                {/*<Navbar state = {props.state.partNavbar}/>*/}
+                {/*<Route path='/profile' render = { () => <Profile state = {props.state.pageProfile} /> }/>*/}
+                {/*<Route path='/messenger' render = { () => <Dialogs state = {props.state.pageMessenger} /> }/>*/}
                 {/* <Route path='/news'     render={ () => <News/> }/> */}
                 {/* <Route path='/music'    render={ () => <Music/> }/> */}
                 {/* <Route path='/settings' render={ () => <Settings/> }/> */}
                 {/* <Footer/> */}
-=======
-                <Header 
-                    headerData={props.headerData} 
-                    headerSocialLinksData={props.headerSocialLinksData} 
-                />
-
-                <Navbar/>
-                <Route path='/profile'  render={ () => <Profile
-                    galeryData={props.galeryData}
-                    aboutData={props.aboutData}
-                    submitPostData={props.submitPostData}
-                    postsData={props.postsData}
-                /> }/>
-                <Route path='/dialogs'  render={ () => <Dialogs 
-                    DialogsData={props.DialogsData} 
-                    MessagesData={props.MessagesData}
-                />}/>
-                <Route path='/news'     render={ () => <News/> }/>
-                <Route path='/music'    render={ () => <Music/> }/>
-                <Route path='/settings' render={ () => <Settings/> }/>
-                <Footer/>
->>>>>>> parent of dae5651... 29 lesson - state
             </div>
         </BrowserRouter>
     );
